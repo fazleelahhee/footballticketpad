f2e0e2e8d0aef7ad053099e46bba904383247902
abdul samad <as@bondmedia.co.uk>
2014-08-14 12:07:31 +0100

    no message

--- app/views/frontend/bm2014/_layout/header.blade.php
+++ app/views/frontend/bm2014/_layout/header.blade.php
@@ -8,6 +8,14 @@
         <a href="/"><img class="logo" src="{{ Assets::Path('images/main_logo.png') }}" alt="Football Ticket Pad" /></a>
         
         
+        
+        
+        
+        
+        
+        
+        
+        
         <span class="menu-container">
             <span class="menubtn">
               
@@ -19,12 +27,7 @@
         </span>
     
         
-         <div class="searcharea innersearch">
-                <form id="form-home">
-                    <input class="forminput" placeholder="eg. Champions League Final" >
-                    <input class="bigbox-search-button" type="button"></button>
-                </form>
-         </div>
+         
         
         
         
@@ -33,7 +36,18 @@
         <a href="/"><span class="btn loginbtn">LOGIN</span></a>
         
        
-        <a href="/"><span class="btn myaccount">MY ACCOUNT</span></a>        
+        <a href="/"><span class="btn myaccount">MY ACCOUNT</span></a> 
+        
+        
+        
+        <div class="searcharea innersearch">
+                <form id="form-home">
+                    <input class="forminput" placeholder="eg. Champions League Final" >
+                    <input class="bigbox-search-button" type="button"></button>
+                </form>
+         </div>
+        
+               
         
     </div>
 
