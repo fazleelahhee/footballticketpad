e82c1ec51ae2032bb82a2fd8de4ebe56cdbec9a3
abdul samad <as@bondmedia.co.uk>
2014-08-14 19:15:26 +0100

    kfc

---
+++ app/views/frontend/bm2014/account.blade.php
@@ -0,0 +1,41 @@
+@extends(Template::name('frontend.%s._layout.layout'))
+@section('style')
+{{ HTML::style('ckeditor/contents.css') }}
+@stop
+@section('content')
+
+
+
+<section class="banner-home">
+
+ 	
+    <img class="inner-banner" src="{{ Assets::Path('images/account.jpg') }}" alt="test" />
+    
+    <span class="ftp-line">
+    	<span class="greenline"></span>
+        <span class="yellowline"></span>
+        <span class="pinkline"></span>
+        <span class="blueline"></span>
+    </span>	
+    
+    
+</section>
+
+
+
+
+<!---------main content------------>
+<div class="row">
+
+	 <h1 class="page-header">{{ e($node->title) }}</h1>   
+
+     <div class="site-content">
+                     
+        {{ $node->content }}          
+                            
+     </div>
+</div>
+<!---------main content------------>
+
+
+@stop
