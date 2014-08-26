ab6cf8992cbed7ec71c538ae8b047f42d37a2677
Fazle Elahee <felahee@gmail.com>
2014-08-26 18:41:09 +0100

    disable csrf

--- app/routes.php
+++ app/routes.php
@@ -219,7 +219,9 @@ Route::group(array('namespace' => 'App\Controllers\Admin', 'before' => 'assets_a
 | Football tickets
 |--------------------------------------------------------------------------
 */
-Route::post('/ticket/registration', array('before' => 'csrf', 'as' => 'ticket.registrations', 'uses' => 'CustomerController@registrationAction'));
+//freaking does not work properly with session
+//Route::post('/ticket/registration', array('before' => 'csrf', 'as' => 'ticket.registrations', 'uses' => 'CustomerController@registrationAction'));
+Route::post('/ticket/registration', array( 'as' => 'ticket.registrations', 'uses' => 'CustomerController@registrationAction'));
 /*
 |--------------------------------------------------------------------------
 | General Routes
