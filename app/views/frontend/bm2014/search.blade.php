1903882c7efc9b0c91058e896f4211a083a0e4ea
Fazle Elahee <felahee@gmail.com>
2014-08-27 13:00:26 +0100

    changes

--- app/views/frontend/bm2014/search.blade.php
+++ app/views/frontend/bm2014/search.blade.php
@@ -33,42 +33,19 @@
               </li>
           </ul>
 
-         <h2 class="event-list-header" >Events <span>(2)</span></h2>
-         <ul class="search-results-list">
-             <li>
-                 <a class=" btn pinkbtn pull-right" href="#">Buy</a>
-                 <a href="#"><span class="game"> Manchester City</span></a>
-                 <span class="date-result">04 November 19:00</span>
-                 <span class="subtext"><a href="#">O2 Apollo Manchester</a></span>
-             </li>
-             <li>
-                 <a class=" btn pinkbtn pull-right" href="#">Buy</a>
-                 <a href="#"><span class="game"> Manchester City</span></a>
-                 <span class="date-result">04 November 19:00</span>
-                 <span class="subtext"><a href="#">O2 Apollo Manchester</a></span>
-             </li>
-             <li>
-                 <a class=" btn pinkbtn pull-right" href="#">Buy</a>
-                 <a href="#"><span class="game"> Manchester City</span></a>
-                 <span class="date-result">04 November 19:00</span>
-                 <span class="subtext"><a href="#">O2 Apollo Manchester</a></span>
-             </li>
-             <li>
-                 <a class=" btn pinkbtn pull-right" href="#">Buy</a>
-                 <a href="#"><span class="game"> Manchester City</span></a>
-                 <span class="date-result">04 November 19:00</span>
-                 <span class="subtext"><a href="#">O2 Apollo Manchester</a></span>
-             </li>
-             <li>
-                 <a class=" btn pinkbtn pull-right" href="#">Buy</a>
-                 <a href="#"><span class="game"> Manchester City</span></a>
-                 <span class="date-result">04 November 19:00</span>
-                 <span class="subtext"><a href="#">O2 Apollo Manchester</a></span>
-             </li>
+         <h2 class="event-list-header" >Events <span></span></h2>
+         <ul class="search-results-list event-ticket">
+
          </ul>
      </div>
 </div>
 @stop
+{{
+    Assets::setStyles(
+    [
+        'loading'         => 'css/ajax-loading.css'
+    ], false, true);
+}}
 
 {{
     Assets::setScripts(
@@ -82,9 +59,53 @@
 {{ Assets::jsStart() }}
 <script type="text/javascript" charset="utf-8">
 
+    (function ($) {
+        var ticketContainer =  $('body');
+        //var template = _.template($("#ticket-element-template").html());
+
+        $(document).ready(function () {
+            $.ajax({
+                url: '{{$ticketApi}}api/rest/products?order=name{{$query_param}}',
+                type: 'GET',
+                dataType: 'json',
+                beforeSend: function (xhr) {
+                    $(".ajax-loading-modal").remove();
+                    ticketContainer.append('<div class="ajax-loading-modal"></div>');
+                    ticketContainer.addClass("loading");
+                },
+                success: function (response) {
+                    var i = 0;
+                    _.each(response, function (item) {
+                        if(item.entity_id) {
+//                            var html = template({
+//                                title: '',
+//                                event_date: '04 November 19:00',
+//                                short_text: ''
+//                            });
+                            var html = '<li><a class=" btn pinkbtn pull-right" href="#">Buy</a>';
+                            html += '<a href="#"><span class="game">'+item.name+'</span></a>';
+                            html += '<span class="date-result">04 November 19:00</span>';
+                            html += '<span class="subtext"><a href="#">'+item.description+'</a></span></li>';
+
+                            $('.event-ticket').append(html);
+                            i++;
+                        }
+                    });
+
+                    if(i === 0) {
+                        $('.event-ticket').append('<li><span class="subtext">No ticket found</span></li>');
+                    } else {
+                        $('.event-list-header span').html('('+i+')');
+                    }
+
+                    ticketContainer.removeClass('loading');
+                }
+            });
+        });
+    })(jQuery)
 </script>
 
-<script type="text/x-template" charset="utf-8">
+<script type="text/x-template" charset="utf-8" id="ticket-element-template">
     <li>
         <a class=" btn pinkbtn pull-right" href="#">Buy</a>
         <a href="#"><span class="game"><%= title %></span></a>
