0c2c23f132b4fc4cec6fa9ac5be76a45ffcf6c7b
abdul samad <as@bondmedia.co.uk>
2014-09-18 18:53:55 +0100

    no message

--- app/views/frontend/bm2014/news/show.blade.php
+++ app/views/frontend/bm2014/news/show.blade.php
@@ -49,11 +49,7 @@
 
             {{ $news->content }}
 
-            <!-- Go to www.addthis.com/dashboard to customize your tools -->
-            <div class="addthis_sharing_toolbox"></div>
-            <!-- Go to www.addthis.com/dashboard to customize your tools -->
-            <script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4f426a07524dc6b4"></script>
-
+           
 
 
 
